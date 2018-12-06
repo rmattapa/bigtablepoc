0e608199b8276dc383a86a9b11e64513ad1dc354
Rajesh <rmattapa@asu.edu>
2018-12-06 17:12:37 -0500

    updated code

--- src/main/java/AvroBigTable.java
+++ src/main/java/AvroBigTable.java
@@ -1,4 +1,24 @@
-package Samples;
+import com.google.common.base.Preconditions;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.AvroIO;
+import org.apache.beam.sdk.io.FileSystems;
+import org.apache.beam.sdk.options.Description;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.options.Validation;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.hadoop.hbase.client.Mutation;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.beam.sdk.io.TextIO;
+
+import com.google.cloud.bigtable.beam.CloudBigtableIO;
+import com.google.cloud.bigtable.beam.CloudBigtableTableConfiguration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,200 +26,139 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
-import java.util.Arrays;
-import java.util.List;
-import options.SampleOptions;
-import org.apache.avro.Schema;
-import org.apache.avro.file.CodecFactory;
-import org.apache.avro.generic.GenericData;
-import org.apache.avro.generic.GenericRecord;
-import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.coders.AvroCoder;
-import org.apache.beam.sdk.io.AvroIO;
-import org.apache.beam.sdk.io.FileSystems;
-import org.apache.beam.sdk.io.TextIO;
-import org.apache.beam.sdk.testing.TestPipeline;
-import org.apache.beam.sdk.transforms.DoFn;
-import org.apache.beam.sdk.transforms.ParDo;
-import org.apache.beam.sdk.transforms.Sum;
-import org.apache.beam.sdk.values.KV;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-/**
- * Sample transformations on PCollections like converting csv files to avro.
- * Transformations on PCollections of Generic avro records.
- */
-public class DataTransforms {
+public class AvroToBigTable
+{
+	private static final byte[] FAMILY = Bytes.toBytes("csv");
+	private static final Logger LOG = LoggerFactory.getLogger(AvroToBigTable.class);
 
-    private static final Logger LOG = LoggerFactory.getLogger(DataTransforms.class);
-    private static final List<String> acceptedTypes = Arrays.asList(
-        new String[]{"string", "boolean", "int", "long", "float", "double"});
+	static final DoFn<String, Mutation> MUTATION_TRANSFORM = new DoFn<String, Mutation>() {
+		@ProcessElement
+		public void processElement(DoFn<String, Mutation>.ProcessContext c) throws Exception {
+			try {
+				String[] headers = c.getPipelineOptions().as(BigtableCsvOptions.class).getHeaders()
+									.split(",");
+				String[] values = c.element().split(",");
+				Preconditions.checkArgument(headers.length == values.length);
 
-    /**
-     * Read the avro schema from the given path.
-     */
-    public static String getSchema(String schemaPath) throws IOException {
-        ReadableByteChannel chan = FileSystems.open(FileSystems.matchNewResource(
-            schemaPath, false));
+				byte[] rowkey = Bytes.toBytes(values[0]);
+				byte[][] headerBytes = new byte[headers.length][];
+				for (int i = 0; i < headers.length; i++) {
+					headerBytes[i] = Bytes.toBytes(headers[i]);
+				}
 
-        try (InputStream stream = Channels.newInputStream(chan)) {
-            BufferedReader streamReader = new BufferedReader(new InputStreamReader(stream, "UTF-8"));
-            StringBuilder dataBuilder = new StringBuilder();
+				Put row = new Put(rowkey);
+				long timestamp = System.currentTimeMillis();
+				for (int i = 1; i < values.length; i++) {
+					row.addColumn(FAMILY, headerBytes[i], timestamp, Bytes.toBytes(values[i]));
+				}
+				c.output(row);
+			} catch (Exception e) {
+				LOG.error("Failed to process input {}", c.element(), e);
+				throw e;
+			}
 
-            String line;
-            while ((line = streamReader.readLine()) != null) {
-                dataBuilder.append(line);
-            }
+		}
+	};
 
-            return dataBuilder.toString();
-        }
-    }
 
-    /**
-     * Method to verify field types in an avro schema.
-     */
-    public static void checkFieldTypes(Schema schema) throws IllegalArgumentException {
-        for (Schema.Field field : schema.getFields()) {
-            String fieldType = field.schema().getType().getName().toLowerCase();
-            if (!acceptedTypes.contains(fieldType)) {
-                LOG.error("Data transformation doesn't support: " + fieldType);
-                throw new IllegalArgumentException("Field type " + fieldType + " is not supported.");
-            }
-        }
-    }
+	public static interface BigtableCsvOptions extends CloudBigTableOptions {
 
-    /**
-     * Method to convert csv files to avro using beam transformations.
-     */
-    public static void runCsvToAvro(SampleOptions options)
-        throws IOException, IllegalArgumentException {
-        FileSystems.setDefaultPipelineOptions(options);
+		@Description("The headers for the avro file.")
+		String getHeaders();
 
-        // Get Avro Schema
-        String schemaJson = getSchema(options.getAvroSchema());
-        Schema schema = new Schema.Parser().parse(schemaJson);
+		void setHeaders(String headers);
 
-        // Check schema field types before starting the Dataflow job
-        checkFieldTypes(schema);
+		@Description("The Cloud Storage path to the avro file..")
+		String getInputFile();
 
-        // Create the Pipeline object with the options we defined above.
-        Pipeline pipeline = Pipeline.create(options);
+		void setInputFile(String location);
 
-        // Convert CSV to Avro
-        pipeline.apply("Read CSV files", TextIO.read().from(options.getInputFile()))
-            .apply("Convert CSV to Avro formatted data",
-                ParDo.of(new ConvertCsvToAvro(schemaJson, options.getCsvDelimiter())))
-            .setCoder(AvroCoder.of(GenericRecord.class, schema))
-            .apply("Write Avro formatted data", AvroIO.writeGenericRecords(schemaJson)
-                .to(options.getOutput()).withCodec(CodecFactory.snappyCodec()).withSuffix(".avro").withoutSharding());
+		@Description(
+				"Set output required parameter to define output path. A local path and Google Cloud Storage"
+						+ " path are both supported.")
+		String getOutput();
 
-        // Run the pipeline.
-        pipeline.run().waitUntilFinish();
-    }
+		void setOutput(String value);
 
-    /**
-     * method to convert csv to avro to a batch data.
-     */
-    public static void transformGenericAvro(SampleOptions options) throws IOException {
-        // Create the Pipeline object with the options we defined above.e
-        Pipeline pipeline = Pipeline.create(options);
+		/**
+		 * Set avroSchema required parameter to specify location of the schema. A local path and Google
+		 * Cloud Storage path are supported.
+		 */
+		@Description(
+				"Set avroSchema required parameter to specify location of the schema. A local path and "
+						+ "Google Cloud Storage path are supported.")
+		String getAvroSchema();
 
-        // Get Avro Schema
-        String schemaJson = getSchema(options.getAvroSchema());
-        Schema schema = new Schema.Parser().parse(schemaJson);
+		void setAvroSchema(String value);
 
-        // Read GenericRecord's of the given schema from files on GCS
-        pipeline.apply("GetAvro", AvroIO.readGenericRecords(schema)
-            .from("C:\\Users\\axc353\\Documents\\Git\\indian-premier-league-csv-dataset\\Player.avro"))
-            .apply("Batting-style-count", ParDo.of(new DoFn<GenericRecord, KV<String, Integer>>() {
-                @ProcessElement
-                public void processElement(ProcessContext c) throws Exception {
-                    String key = c.element().get("Batting_Hand").toString();
-                    c.output(KV.of(key, 1));
-                }
-            }))
-            .apply(Sum.integersPerKey())
-            .apply("ToString", ParDo.of(new DoFn<KV<String, Integer>, String>() {
+		@Description(
+				"Set avroFile required parameter to specify location of the avro file. A local path and "
+						+ "Google Cloud Storage path are supported.")
+		String getAvroFile();
 
-                @ProcessElement
-                public void processElement(ProcessContext c) throws Exception {
-                    StringBuffer sb = new StringBuffer();
-                    sb.append(c.element().getKey() + "," + c.element().getValue() + '\n');
+		void setAvroFile(String value);
+	}
 
-                    c.output(sb.toString());
-                }
-            }))
-            .apply(TextIO.write().to(options.getOutput()).withSuffix(".txt").withoutSharding());
-        // Run the pipeline.
-        pipeline.run().waitUntilFinish();
-    }
+	public static String getSchema(String schemaPath) throws IOException
+	{
+		ReadableByteChannel chan = FileSystems.open(FileSystems.matchNewResource(
+				schemaPath, false));
 
-    public static void main(String[] args) throws IOException, IllegalArgumentException {
-        // Create and set your PipelineOptions.
-        SampleOptions options = TestPipeline.testingPipelineOptions().as(SampleOptions.class);
-        options.setAvroSchema("resources/user.avsc");
-        options.setInputFile("gs://my-test-data-abhishek-1/sample/Player.csv");
-        options.setOutput("gs://my-test-data-abhishek-1/sample/Player");
+		try (InputStream stream = Channels.newInputStream(chan)) {
+			BufferedReader streamReader = new BufferedReader(new InputStreamReader(stream, "UTF-8"));
+			StringBuilder dataBuilder = new StringBuilder();
 
-        runCsvToAvro(options);
-        transformGenericAvro(options);
-    }
+			String line;
+			while ((line = streamReader.readLine()) != null) {
+				dataBuilder.append(line);
+			}
 
-    /**
-     * class to process csv to avro generic record transformations.
-     */
-    public static class ConvertCsvToAvro extends DoFn<String, GenericRecord> {
+			return dataBuilder.toString();
+		}
+	}
 
-        private String delimiter;
-        private String schemaJson;
+	public static void main(String[] args) throws IOException
+	{
+		BigtableCsvOptions options =
+				PipelineOptionsFactory.fromArgs(args).as(BigtableCsvOptions.class);
 
-        public ConvertCsvToAvro(String schemaJson, String delimiter) {
-            this.schemaJson = schemaJson;
-            this.delimiter = delimiter;
-        }
+		options.setAvroSchema("src/main/resources/twitter.avsc");
+		options.setAvroFile("src/main/resources/twitter.avro");
+		options.setOutput("C:/projects/bigtablepoc/src/main/resources/");
+		options.setBigtableProjectId("sample");
+		options.setBigtableInstanceId("sample");
+		options.setBigtableTableId("sample");
 
-        @ProcessElement
-        public void processElement(ProcessContext ctx) throws IllegalArgumentException {
-            // Split CSV row into using delimiter
-            String[] rowValues = ctx.element().split(delimiter);
+		CloudBigtableTableConfiguration config =
+				new CloudBigtableTableConfiguration.Builder()
+						.withProjectId(options.getBigtableProjectId())
+						.withInstanceId(options.getBigtableInstanceId())
+						.withTableId(options.getBigtableTableId())
+						.build();
 
-            Schema schema = new Schema.Parser().parse(schemaJson);
+		String schemaJson = getSchema(options.getAvroSchema());
+		Schema schema = new Schema.Parser().parse(schemaJson);
 
-            // Create Avro Generic Record
-            GenericRecord genericRecord = new GenericData.Record(schema);
-            List<Schema.Field> fields = schema.getFields();
+		Pipeline p = Pipeline.create(options);
 
-            for (int index = 0; index < fields.size(); ++index) {
-                Schema.Field field = fields.get(index);
-                String fieldType = field.schema().getType().getName().toLowerCase();
+		p.apply("ReadingAvro",AvroIO.readGenericRecords(schema).from(options.getAvroFile()))
+		 .apply("convertAvroToString",ParDo.of(new DoFn<GenericRecord, String>()
+		 {
+		 	@ProcessElement
+			 public void convertString(ProcessContext c) throws Exception {
+		 		String timestamp = c.element().get("timestamp").toString();
+		 		String username = c.element().get("username").toString();
+		 		String tweet = c.element().get("tweet").toString();
+		 		StringBuffer sb = new StringBuffer();
+		 		sb.append(timestamp + "," + username + "," + tweet);
+		 		c.output(sb.toString());
+			}
+		 }))
+//		.apply(TextIO.write().to(options.getOutput()).withSuffix(".txt").withoutSharding())
+		 .apply("TransformParsingsToBigtable", ParDo.of(MUTATION_TRANSFORM))
+		 .apply("WriteToBigtable", CloudBigtableIO.writeToTable(config));
 
-                switch (fieldType) {
-                    case "string":
-                        genericRecord.put(field.name(), rowValues[index]);
-                        break;
-                    case "boolean":
-                        genericRecord.put(field.name(), Boolean.valueOf(rowValues[index]));
-                        break;
-                    case "int":
-                        genericRecord.put(field.name(), Integer.valueOf(rowValues[index]));
-                        break;
-                    case "long":
-                        genericRecord.put(field.name(), Long.valueOf(rowValues[index]));
-                        break;
-                    case "float":
-                        genericRecord.put(field.name(), Float.valueOf(rowValues[index]));
-                        break;
-                    case "double":
-                        genericRecord.put(field.name(), Double.valueOf(rowValues[index]));
-                        break;
-                    default:
-                        LOG.error("Data transformation doesn't support: " + fieldType);
-                        throw new IllegalArgumentException("Field type " + fieldType + " is not supported.");
-                }
-            }
-            ctx.output(genericRecord);
-        }
-    }
+		p.run().waitUntilFinish();
+	}
 }
-
