e629e0fa92e1c0fa16e6b15d7accef30ce05c598
Rajesh <rmattapa@asu.edu>
2018-12-06 17:13:16 -0500

    updated options

--- src/main/java/CloudBigTableOptions.java
+++ src/main/java/CloudBigTableOptions.java
@@ -1,58 +1,20 @@
-package options;
-
 import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
-import org.apache.beam.sdk.options.Default;
 import org.apache.beam.sdk.options.Description;
-import org.apache.beam.sdk.options.Validation;
 
-public interface SampleOptions extends DataflowPipelineOptions {
+public interface CloudBigTableOptions extends DataflowPipelineOptions {
+	@Description("The Google Cloud project ID for the Cloud Bigtable instance.")
+	String getBigtableProjectId();
 
-    /**
-     * Set inuptFile required parameter to a file path or glob. A local path and Google Cloud Storage
-     * path are both supported.
-     */
-    @Description(
-        "Set inuptFile required parameter to a file path or glob. A local path and Google Cloud "
-            + "Storage path are both supported.")
-    @Validation.Required
-    String getInputFile();
+	void setBigtableProjectId(String bigtableProjectId);
 
-    void setInputFile(String value);
+	@Description("The Google Cloud Bigtable instance ID .")
+	String getBigtableInstanceId();
 
-    /**
-     * Set output required parameter to define output path. A local path and Google Cloud Storage path
-     * are both supported.
-     */
-    @Description(
-        "Set output required parameter to define output path. A local path and Google Cloud Storage"
-            + " path are both supported.")
-    @Validation.Required
-    String getOutput();
+	void setBigtableInstanceId(String bigtableInstanceId);
 
-    void setOutput(String value);
+	@Description("The Cloud Bigtable table ID in the instance." )
+	String getBigtableTableId();
 
-    /**
-     * Set avroSchema required parameter to specify location of the schema. A local path and Google
-     * Cloud Storage path are supported.
-     */
-    @Description(
-        "Set avroSchema required parameter to specify location of the schema. A local path and "
-            + "Google Cloud Storage path are supported.")
-    @Validation.Required
-    String getAvroSchema();
+	void setBigtableTableId(String bigtableTableId);
 
-    void setAvroSchema(String value);
-
-    /**
-     * Set csvDelimiter optional parameter to specify the CSV delimiter. Default delimiter is set to a
-     * comma.
-     */
-    @Description(
-        "Set csvDelimiter optional parameter to specify the CSV delimiter. Default delimiter is set"
-            + " to a comma.")
-    @Default.String(",")
-    String getCsvDelimiter();
-
-    void setCsvDelimiter(String delimiter);
 }
-
